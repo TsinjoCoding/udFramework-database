c8b06cb24be9af8dfd0f14a69130eb94bea44cd7
TsinjoAH <52tsinjo@gmail.com>
2023-01-16 22:10:35 +0300

    spring 3.0.1 init

---
+++ SpringDBObject/src/main/java/com/udframework/database/spring/SpringDbObjectApplication.java
@@ -0,0 +1,13 @@
+package com.udframework.database.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDbObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDbObjectApplication.class, args);
+	}
+
+}
---
+++ SpringDBObject/src/test/java/com/udframework/database/spring/SpringDbObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udframework.database.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
